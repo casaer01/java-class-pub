a1c1c599d086370a6a81442b2c5c95a9aaf5aa08
casaer01 <casaer01@10.28.7.198>
2017-11-14 12:53:20 -0600

    completed book, college, company, employee, room

--- src/exercise6/Crow.java
+++ src/exercise6/Crow.java
@@ -1,8 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package exercise6;
 
+import java.util.Objects;
+
 /**
- * Class Crow
- * @author yasiro01
+ *
+ * @author casaer01
  */
 public class Crow {
+    private String name;
+    private String age;
+    private int iq = 100;
+
+    public Crow(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    @Override
+    public int hashCode() {
+        char[] nameChar = name.toCharArray();
+        char[] ageChar = age.toCharArray();
+        int total = 0;
+        
+        for (int i = 0; i < nameChar.length; i++){
+            int ascii = nameChar[i];
+            total += ascii * i;
+        }
+        for (int i = 0; i < ageChar.length; i++) {
+            int ascii = ageChar[i];
+            total += ascii * i;
+        }
+        return total + iq;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Crow other = (Crow) obj;
+        if (this.iq != other.iq) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.age, other.age)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Crow{" + "name=" + name + ", age=" + age + ", iq=" + iq + '}';
+    }
+        
+    
 }
--- src/exercise6/Kangaroo.java
+++ src/exercise6/Kangaroo.java
@@ -1,8 +1,72 @@
 package exercise6;
 
+import java.util.Objects;
+
 /**
  * Class Kangaroo
- * @author yasiro01
+ * @author casaer01
+ * String name, integer age, and integer babies (1 by default
  */
 public class Kangaroo {
+    private String name;
+    private int age;
+    private int babies = 1;
+
+    public Kangaroo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getBabies() {
+        return babies;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + this.age;
+        hash = 29 * hash + this.babies;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Kangaroo other = (Kangaroo) obj;
+        if (this.age != other.age) {
+            return false;
+        }
+        if (this.babies != other.babies) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Kangaroo{" + "name=" + name + ", age=" + age + ", babies=" + babies + '}';
+    }
+    
+    
+    
 }
