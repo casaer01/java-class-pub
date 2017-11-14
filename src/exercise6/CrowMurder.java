c6992c871a1a5d1ccdf3be14165c5d84beba4cae
casaer01 <casaer01@10.28.7.198>
2017-11-14 12:53:20 -0600

    completed book, college, company, employee, room

--- src/exercise6/CrowMurder.java
+++ src/exercise6/CrowMurder.java
@@ -1,8 +1,38 @@
 package exercise6;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Class CrowMurder
  * @author yasiro01
  */
 public class CrowMurder {
+    private final ArrayList<Crow> crowList;
+    
+    public CrowMurder (String filename) throws FileNotFoundException, IOException {
+        crowList = new ArrayList();
+        BufferedReader inputFile = new BufferedReader(new FileReader(filename));
+        String line;
+        while ((line = inputFile.readLine()) != null) {
+        /* Turn each line into an array of Strings and print it */
+        String[] lineContent = line.split(",");
+        for (String item: lineContent) {
+          if(lineContent[3] == "Frog"){
+              crowList.add(new Crow(Integer.parseInt));
+          }
+        }
+    }
+        
+        
+}
+    public int size(){
+        return crowList.size();
+    }
+    
 }
--- src/exercise6/Frog.java
+++ src/exercise6/Frog.java
@@ -1,8 +1,73 @@
 package exercise6;
 
+import java.util.Objects;
+
 /**
  * Class Frog
- * @author yasiro01
+ * @author casaer01
  */
 public class Frog {
+    private String name;
+    private int age;
+    private String color = "Green";
+
+    public Frog(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + this.age;
+        hash = 67 * hash + Objects.hashCode(this.color);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Frog other = (Frog) obj;
+        if (this.age != other.age) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.color, other.color)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Frog{" + "name=" + name + ", age=" + age + ", color=" + color + '}';
+    }
+    
+    
+    
+    
 }
