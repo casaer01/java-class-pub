ad2beb374e70fa254f315b6b880354bdafc71289
casaer01 <casaer01@10.28.7.198>
2017-11-28 12:46:32 -0600

    exercise6 basic

--- src/exercise6/Crow.java
+++ src/exercise6/Crow.java
@@ -1,8 +1,71 @@
 package exercise6;
 
+import java.util.Objects;
+
 /**
  * Class Crow
  * @author yasiro01
  */
 public class Crow {
+    private String name;
+    private int age;
+    private int iq = 100;
+
+    public Crow(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    @Override
+    public String toString() {
+        return "Crow{" + "name=" + name + ", age=" + age + ", iq=" + iq + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + this.age;
+        hash = 67 * hash + this.iq;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Crow other = (Crow) obj;
+        if (this.age != other.age) {
+            return false;
+        }
+        if (this.iq != other.iq) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+            
 }
--- src/exercise6/FrogArmy.java
+++ src/exercise6/FrogArmy.java
@@ -1,8 +1,35 @@
 package exercise6;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Class FrogArmy
  * @author yasiro01
  */
 public class FrogArmy {
+    private String filename;
+    private final ArrayList<Frog> frogList;
+
+    public FrogArmy(String filename) throws FileNotFoundException, IOException {
+        this.filename = filename;
+        frogList = new ArrayList();
+        
+        BufferedReader inputFile = new BufferedReader(new FileReader(filename));
+        String line;
+        while ((line = inputFile.readLine()) != null) {
+            /* Turn each line into an array of Strings and print it */
+            String[] lineContent = line.split(",");
+            for (String item: lineContent) {
+              //placeholder
+            }
+        }
+    }
+    
+    
+    
+    
+    
 }
