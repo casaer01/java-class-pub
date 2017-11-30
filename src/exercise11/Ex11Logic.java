0f8c41e15f955f0072eeb2f3dd55a5378fc83623
casaer01 <casaer01@10.28.7.198>
2017-11-30 14:22:56 -0600

    Ex11 task 2/5 done

--- src/exercise11/Ex11Logic.java
+++ src/exercise11/Ex11Logic.java
@@ -7,47 +7,64 @@ import java.util.Collection;
  * @author yasiro01
  */
 public class Ex11Logic {
-  
-  /**
-   * Task 1
-   * @param <T>
-   * @param item
-   * @return length of the parameter's string representation
-   */
-  // TODO: Implement method strLen
-  
-  /**
-   * Task 2
-   * @param <T>
-   * @param item1
-   * @param item2
-   * @param item3
-   * @return the greater of three parameters
-   */
-  // TODO: Implement method greater
-  
-  /**
-   * Task 3
-   * @param <T>
-   * @param list of objects
-   * print a list of items
-   */
-  // TODO: Implement method printList
-  
-  /**
-   * Task 4
-   * @param <T>
-   * @param list of objects
-   * @return the smallest item in the collection
-   */
-  // TODO: Implement method findMinItem
-}
 
-  /**
-   * Task 5
-   * @param <T>
-   * @param <U>
-   * A class with two data members, T fuzz and U buzz
-   * Implement the constructor, getters, setters, and toString
-   */
-  // TODO: Implement class Foo
+    /**
+    * Task 1
+    * @param <T>
+    * @param item
+    * @return length of the parameter's string representation
+    */
+    // TODO: Implement method strLen
+    public <T> int strLen (T item){
+        return item.toString().length();
+    }
+    /**
+    * Task 2
+    * @param <T>
+    * @param item1
+    * @param item2
+    * @param item3
+    * @return the greater of three parameters
+    */
+    // TODO: Implement method greater
+    public <T extends Comparable> int greater(T item1, T item2, T item3) {
+        if (item1.compareTo(item2) >= 0){
+            return Integer.parseInt(item1.toString());
+        } else if (item1.compareTo(item3) >= 0) {
+            return Integer.parseInt(item2.toString());
+        } else if (item2.compareTo(item1) >= 0) {
+            return Integer.parseInt(item2.toString());
+        } else if (item2.compareTo(item3) >= 0) {
+            return Integer.parseInt(item2.toString());
+        } else if (item3.compareTo(item1) >= 0) {
+            return Integer.parseInt(item3.toString());
+        } else {
+            return Integer.parseInt(item3.toString());
+        }
+    }
+
+    /**
+    * Task 3
+    * @param <T>
+    * @param list of objects
+    * print a list of items
+    */
+    // TODO: Implement method printList
+
+    /**
+    * Task 4
+    * @param <T>
+    * @param list of objects
+    * @return the smallest item in the collection
+    */
+    // TODO: Implement method findMinItem
+    }
+
+    /**
+    * Task 5
+    * @param <T>
+    * @param <U>
+    * A class with two data members, T fuzz and U buzz
+    * Implement the constructor, getters, setters, and toString
+    */
+    // TODO: Implement class Foo
