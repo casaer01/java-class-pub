85719aa06945a3d652b62f113740d588f4c8f696
casaer01 <casaer01@10.28.7.198>
2017-12-01 14:50:39 -0600

    1 out of 5 task done

--- src/exercise11/Ex11Logic.java
+++ src/exercise11/Ex11Logic.java
@@ -1,13 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package exercise11;
 
 import java.util.Collection;
 
 /**
- * Exercise 11 logic
- * @author yasiro01
+ *
+ * @author casaer01
  */
 public class Ex11Logic {
-
+    
     /**
     * Task 1
     * @param <T>
@@ -17,7 +22,7 @@ public class Ex11Logic {
     // TODO: Implement method strLen
     public <T> int strLen (T item){
         return item.toString().length();
-    }
+}
     /**
     * Task 2
     * @param <T>
@@ -27,19 +32,19 @@ public class Ex11Logic {
     * @return the greater of three parameters
     */
     // TODO: Implement method greater
-    public <T extends Comparable> int greater(T item1, T item2, T item3) {
+    public <T extends Comparable> T greater(T item1, T item2, T item3) {
         if (item1.compareTo(item2) >= 0){
-            return Integer.parseInt(item1.toString());
+            return item1;
         } else if (item1.compareTo(item3) >= 0) {
-            return Integer.parseInt(item2.toString());
+            return item1;
         } else if (item2.compareTo(item1) >= 0) {
-            return Integer.parseInt(item2.toString());
+            return item2;
         } else if (item2.compareTo(item3) >= 0) {
-            return Integer.parseInt(item2.toString());
+            return item2;
         } else if (item3.compareTo(item1) >= 0) {
-            return Integer.parseInt(item3.toString());
+            return item3;
         } else {
-            return Integer.parseInt(item3.toString());
+            return item3;
         }
     }
 
@@ -50,8 +55,8 @@ public class Ex11Logic {
     * print a list of items
     */
     // TODO: Implement method printList
-    public <T> void printList(Collection<T> coll){
-        for (T item: coll) {
+    public <T> void printList(T ... list){
+        for (T item: list) {
             System.out.println(item);
         }
     }
@@ -63,8 +68,8 @@ public class Ex11Logic {
     * @return the smallest item in the collection
     */
     // TODO: Implement method findMinItem
-    public <T> String findMinItem (Collection<T> coll) {
-        for (T item: coll) {
+    public <T> String findMinItem (T ... list) {
+        for (T item: list) {
             
         }
         return "null";
@@ -79,3 +84,34 @@ public class Ex11Logic {
     * Implement the constructor, getters, setters, and toString
     */
     // TODO: Implement class Foo
+
+class Foo <T, U> {
+    private T fuzz;
+    private U buzz;
+
+    public Foo(T fuzz, U buzz) {
+        this.fuzz = fuzz;
+        this.buzz = buzz;
+    }
+
+    public T getFuzz() {
+        return fuzz;
+    }
+
+    public void setFuzz(T fuzz) {
+        this.fuzz = fuzz;
+    }
+
+    public U getBuzz() {
+        return buzz;
+    }
+
+    public void setBuzz(U buzz) {
+        this.buzz = buzz;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" + "fuzz=" + fuzz + ", buzz=" + buzz + '}';
+    }
+}
