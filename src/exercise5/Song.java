a6f500e11a79764892d670f0318e958ba810a113
casaer01 <casaer01@10.28.7.198>
2017-12-06 13:53:45 -0600

    attempted exercise

--- src/exercise5/Song.java
+++ src/exercise5/Song.java
@@ -1,5 +1,7 @@
 package exercise5;
 
+import java.util.Objects;
+
 /**
  * Class Song
  * Task 7: Create a class Song with the following private data members: String title, String artist.
@@ -8,5 +10,53 @@ package exercise5;
  * @author yasiro01
  */
 public class Song {
-  
+    private String title;
+    private String artist;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    @Override
+    public int hashCode() {
+        String[] titleArr = title.split("");
+        String[] artistArr = artist.split("");
+        int charValue = 0;
+        for (int i = 0; i < title.length();i++){
+            charValue = titleArr[i].charAt(i) * i;
+        }
+        return charValue;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Song other = (Song) obj;
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.artist, other.artist)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
