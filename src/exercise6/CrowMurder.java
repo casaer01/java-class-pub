fa2e3875f9f3df980420c0426b36cc04b6ec4707
casaer01 <casaer01@10.28.7.198>
2017-12-07 15:41:14 -0600

    attempted exercise

--- src/exercise6/CrowMurder.java
+++ src/exercise6/CrowMurder.java
@@ -4,25 +4,46 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Class CrowMurder
- * @author yasiro01
+ * @author casaer01
  */
 public class CrowMurder {
-    private String filename;
+    private final ArrayList<Crow> crowList;
 
     public CrowMurder(String filename) throws FileNotFoundException, IOException {
-        this.filename = filename;
+        crowList = new ArrayList();
         BufferedReader inputFile = new BufferedReader(new FileReader(filename));
         String line;
         while ((line = inputFile.readLine()) != null) {
             String[] lineContent = line.split(",");
             for (String item: lineContent) {
-              ;
+                if ("Crow".equals(lineContent[2])){
+                    crowList.add(new Crow(lineContent[0],Integer.parseInt(lineContent[1])));
+                }
+              
             }
         }
+        for (Crow crow:crowList) {
+            System.out.println(crow);
+        }
+    }
+    public int size() {
+        return crowList.size();
     }
     
+    public Crow getChief() {
+        int minAge = 0;
+        Crow maxCrow = new Crow("crow",0); 
+        for (Crow crow:crowList){
+            if (crow.getAge() > minAge){
+                minAge = crow.getAge();
+                maxCrow = crow;
+            }
+        }
+        return maxCrow;
+    }
     
 }
