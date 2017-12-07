5914ca21eab886df1e6ef36a9cfcab44a3f92a73
casaer01 <casaer01@10.28.7.198>
2017-12-06 21:56:15 -0600

    attempted exercise

--- src/exercise6/CrowMurder.java
+++ src/exercise6/CrowMurder.java
@@ -1,8 +1,28 @@
 package exercise6;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Class CrowMurder
  * @author yasiro01
  */
 public class CrowMurder {
+    private String filename;
+
+    public CrowMurder(String filename) throws FileNotFoundException, IOException {
+        this.filename = filename;
+        BufferedReader inputFile = new BufferedReader(new FileReader(filename));
+        String line;
+        while ((line = inputFile.readLine()) != null) {
+            String[] lineContent = line.split(",");
+            for (String item: lineContent) {
+              ;
+            }
+        }
+    }
+    
+    
 }
